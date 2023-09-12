0730be859ec5bc16cda408129332e6bdc2d11b2a
Rushikesh Acharya <v-racharya@microsoft.com>
2023-09-12 23:23:44 +0530

    feat: add test net configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,5 +4,10 @@ module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
+  },
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/<key>"
+    }
   }
 };
